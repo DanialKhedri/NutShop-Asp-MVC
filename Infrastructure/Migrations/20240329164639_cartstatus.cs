c444c646c61861765a4ec9f38ac015ec7798acb3
Danial Khedri <Danial.kh79@gmail.com>
2024-03-29 20:17:09 +0330

    cart statuse migration

---
+++ Infrastructure/Migrations/20240329164639_cartstatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class cartstatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "cartstatus",
+                table: "Carts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "cartstatus",
+                table: "Carts");
+        }
+    }
+}
