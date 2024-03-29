dffa7d32f07498ff6048e0e358b019ddba8e02c5
Danial Khedri <Danial.kh79@gmail.com>
2024-03-29 20:48:03 +0330

    befor locmig

--- Domain/Entities/Cart/Cart.cs
+++ Domain/Entities/Cart/Cart.cs
@@ -8,23 +8,33 @@ namespace Domain.Entities.Cart
 {
     public class Cart
     {
-
+        #region Properties
         public int Id { get; set; }
 
         public int UserId { get; set; }
 
-
         public List<Product.Product> Products { get; set; }
 
-
-        public int? SumPrice { get; set; }
-
+       
 
         public bool Isdelete { get; set; } = false;
 
         public DateTime CreateDate { get; set; } = DateTime.Now;
 
+
+
         public CartStatus.cartstatus cartstatus { get; set; } = CartStatus.cartstatus.Waiting;
+
+
+
+        public int? SumPrice { get; set; }
+
+        public Location? Location { get; set; }
+
+        #endregion
+
+
+
 
     }
 }
---
+++ Domain/Entities/Cart/Location.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Entities.Cart
+{
+    public class Location 
+    {
+
+        public int Id { get; set; }
+
+        public string? FullName { get; set; }
+
+        public int? Phone { get; set; }
+
+        public string? Email { get; set; }
+
+        public string Address { get; set; }
+
+    }
+}
