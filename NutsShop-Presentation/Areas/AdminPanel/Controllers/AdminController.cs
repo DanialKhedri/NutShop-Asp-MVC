21945ec820a6434ea91c9891174acf888ab0580f
Danial Khedri <Danial.kh79@gmail.com>
2024-04-12 12:43:56 +0330

    admin panel views add

--- NutsShop-Presentation/Areas/AdminPanel/Controllers/AdminController.cs
+++ NutsShop-Presentation/Areas/AdminPanel/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace NutsShop_Presentation.Areas.AdminPanel.Controllers;
 
@@ -6,21 +7,102 @@ namespace NutsShop_Presentation.Areas.AdminPanel.Controllers;
 public class AdminController : Controller
 {
 
+    #region Ctor
+
+    private readonly IProductService _IProductService;
+    private readonly IOrderService _IOrderService;
+    private readonly IUserService _IUserService;
+
+    public AdminController(IProductService productService, IOrderService orderService, IUserService userService)
+    {
+
+        _IProductService = productService;
+        _IOrderService = orderService;
+        _IUserService = userService;
+
+    }
+
+
+    #endregion
+
+
+
+
+
+
+
+
+
+
+
     [HttpGet]
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
         return View();
     }
 
 
 
+    #region Products Managment
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllProducts()
+    {
+
+        return View();
+
+
+    }
+
+    #endregion
+
+
+
+    #region Category Managment
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllCategories()
+    {
+
+
+
+        return View();
+
+    }
+
+    #endregion
+
+
+    #region User Managment
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsers()
+    {
 
 
 
 
 
+        return View();
+
+    }
 
 
+    #endregion
+
+
+    #region Orders Managment
+
+    public async Task<IActionResult> GetAllOrders()
+    {
+
+
+
+
+        return View();
+
+    }
+    #endregion
 
 
 
